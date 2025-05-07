77af907b040805803963e487d3dcda1d5a62cbc6
Kacper Mejsner <kacpermejs@gmail.com>
2025-05-07 18:23:54 +0200

    Main function wireframe

---
+++ main.py
@@ -0,0 +1,22 @@
+# ===================================================
+#     This script runs on remote hadoop machine     #
+# ===================================================
+
+def main():
+  # 0. (Optional) Parse arguments
+  
+  # 1. Download stations_metadata.csv
+
+  # 2. Download air pollution readings
+
+  # 3. Extract lat lon for relevant stations
+
+  # 4. Accumulate Open Meteo API data for historical weather
+
+  # 5. Upload all the data into hadoop
+
+  # 6. Setup dynamic API data in hadoop
+  pass
+
+if __name__ == '__main__':
+  main()
